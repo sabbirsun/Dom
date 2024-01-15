94d178480473ae972347202e906c2cb6e887271c
sabbirsun <sabbirsun1920@gmail.com>
2024-01-15 23:28:28 +0600

    querySelector

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // let ID1 = document.getElementById("ID1");
 // ID1.innerHTML = "I am a Programmer";
 
-let ClassName1 = document.getElementsByClassName("ClassName1")[0];
-ClassName1.innerHTML = "I'm a professional Programmer";
+// let ClassName1 = document.getElementsByClassName("ClassName1")[0];
+// ClassName1.innerHTML = "I'm a professional Programmer";
+
+// let Heading5 = document.getElementsByName("Heading5")[0];
+// Heading5.innerHTML = "Heading 5";
+
+// let H1 = document.getElementsByTagName("h1")[0];
+// H1.innerHTML = "Heading 1 is Change";
+
+// document.querySelector("#ID1");
+// document.querySelector(".ClassName1");
+// document.querySelectorAll("h1");
