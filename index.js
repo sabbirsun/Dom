830a3a0e9a30fa4e72f37bcb9df45fec9d68de07
sabbirsun <sabbirsun1920@gmail.com>
2024-01-15 23:14:35 +0600

    Selecting elements

---
+++ index.js
@@ -0,0 +1,5 @@
+// let ID1 = document.getElementById("ID1");
+// ID1.innerHTML = "I am a Programmer";
+
+let ClassName1 = document.getElementsByClassName("ClassName1")[0];
+ClassName1.innerHTML = "I'm a professional Programmer";
